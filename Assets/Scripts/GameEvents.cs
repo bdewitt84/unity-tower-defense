8f5d4c640062e00fc3d8374ba25faeb54e41162d
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2025-05-03 15:31:52 -0700

    Feature: Game events class added
    Defines events that game objects may broadcast and subscribe to.

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+// Author: Brett DeWitt
+//
+// Date created: 05/03/2025
+//
+// Description:
+// Defines events that game objects may broadcast and subscribe to. Once
+// defined, game objects can subscribe to an event by referencing the
+// definition in this class, ie
+//     GameEvents.DefinedEvent += HandlerFunction
+// The handler function in the subscribing class will be called when
+// the subscribed event is broadcast.
+// 
+// Example:
+//   Definition:
+//      public static Action OnEnemyKilled;
+//      public static void EnemyKilled() => OnEnemyKilled?.Invoke();
+//
+//   Broadcasting:
+//      GameEvents.EnemyKilled(this);
+//
+//   Subscribing:
+//      void OnEnable()
+//      {
+//          GameEvents.OnEnemyKilled += HandleEnemyKilled;
+//      {
+//
+//   Unsubscribing:
+//      void OnDisable()
+//      {
+//          GameEvents.OnEnemyKilled -= HandleEnemyKilled;
+//      }
+
+public static class GameEvents
+{
+    // EnemyKilled
+    public static Action<EnemyController> OnEnemyKilled;
+    public static void EnemyKilled(EnemyController enemy) => OnEnemyKilled?.Invoke(enemy);
+
+    // EnemyReachedGoal
+    public static Action<EnemyController> OnEnemyReachedGoal;
+    public static void EnemyReachedGoal(EnemyController enemy) => OnEnemyReachedGoal?.Invoke(enemy);
+}
