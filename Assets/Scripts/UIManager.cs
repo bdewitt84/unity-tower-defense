fb50862ce660302c7cb1ec89c3a8bf34131540ca
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2025-05-03 18:27:44 -0700

    Feature: Added UI Manager with placeholders

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+// Author: Brett DeWitt
+// 
+// Created: 05.03.2025
+// 
+// Description:
+//    Simple event driven UI manager to update UI components
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text playerHealthText;
+    [SerializeField] private TMP_Text playerGoldText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Add event listners here   
+    }
+
+    // Handlers go here
+}
